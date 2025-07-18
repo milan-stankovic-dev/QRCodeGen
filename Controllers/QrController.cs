c65862ace8920072b8ed2b4af1c7b1ebe2e94f40
DESKTOP-FCGA3DM\milan <milance.za019@gmail.com>
2025-07-18 14:52:04 +0200

    Initial commit

---
+++ Controllers/QrController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace QRCodeGen.Controllers
+{
+    [Route("/api/[controller]")]
+    public class QrController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string url)
+        {
+            using var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrCodeData);
+            var qrCodeBytes = qrCode.GetGraphic(20);
+
+            return File(qrCodeBytes, "image/png");
+        }
+    }
+}
