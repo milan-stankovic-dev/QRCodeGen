e35c50e1cbaed7c4b079bdc282a0cb131652cbef
DESKTOP-FCGA3DM\milan <milance.za019@gmail.com>
2025-07-18 15:22:26 +0200

    Convert to dotnet 8 and fix errors

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Configure the HTTP request pipeline
+//if (app.Environment.IsDevelopment())
+//{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
